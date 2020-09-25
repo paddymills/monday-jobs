b0751ff13b2cea4bcdfbbaa660df8bf99bc3deff
paddymills <paddymills@pm.me>
2020-09-25 17:21:24 -0400

    rename python file; working with calamine

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # monday-jobs
 HSS monday.com jobs board updater
+
+# Rust re-write
+  - [calamine](https://docs.rs/calamine/0.16.1/calamine/)
+  - [chrono?](https://docs.rs/chrono/0.4.16/chrono/index.html)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use calamine::{open_workbook, DataType, Error, RangeDeserializerBuilder, Reader, Xlsx};
+// use chrono::DateTime;
+
+fn example() -> Result<(), Error> {
+    let path = format!("{}/data/__job_ship_dates.xlsx", env!("CARGO_MANIFEST_DIR"));
+    let mut workbook: Xlsx<_> = open_workbook(path)?;
+    let range = workbook
+        .worksheet_range("Dates")
+        .ok_or(Error::Msg("Cannot find sheet 'Dates'"))??;
+    println!("{:?}", range);
+
+    let mut iter = RangeDeserializerBuilder::new().from_range(&range)?;
+
+    if let Some(result) = iter.next() {
+        let (key, early, main): (String, DataType, DataType) = result?;
+        println!("{} | {} | {}", key, early, main);
+        Ok(())
+    } else {
+        Err(From::from("expected at least one record but got none"))
+    }
+}
+
+fn main() -> Result<(), Error> {
+    println!("Rust excel!");
+
+    let _ = example();
+
+    Ok(())
 }
