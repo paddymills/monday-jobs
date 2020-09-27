a2407fda3bff77283634b6b5745e3313623af5a0
Patrick Miller <paddymills@pm.me>
2020-09-27 11:19:04 -0400

    struct use stabilized

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use calamine::{open_workbook, DataType, Xlsx};
 use calamine::{Error, RangeDeserializerBuilder, Reader};
-// use chrono::DateTime;
+use chrono::naive::NaiveDate;
+use regex::Regex;
 
-fn example() -> Result<(), Error> {
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(Debug)]
+struct Job<'a> {
+    name: String,
+    early_start: Option<NaiveDate>,
+    main_start: Option<NaiveDate>,
+    pm: &'a str,
+    bay: &'a str,
+}
+
+// Jobs will live for duration of program
+// so using static shouldn't be an issue
+fn get_data() -> Result<Vec<Job<'static>>, Error> {
     let path = format!("{}/data/test.xlsx", env!("CARGO_MANIFEST_DIR"));
     let mut workbook: Xlsx<_> = open_workbook(path)?;
     let range = workbook
@@ -10,17 +25,38 @@ fn example() -> Result<(), Error> {
         .ok_or(Error::Msg("Cannot find sheet 'Dates'"))??;
 
     let iter = RangeDeserializerBuilder::new().from_range(&range)?;
+    let mut jobs = Vec::<Job>::new();
 
     for elem in iter {
         let (key, early, main): (String, DataType, DataType) = elem?;
-        println!("{:<13} | {:<10} | {}", key, date(early), date(main));
+        match key.as_str() {
+            "" => (),
+            s => jobs.push(Job {
+                name: parse_job(s),
+                early_start: early.as_date(),
+                main_start: main.as_date(),
+                pm: "",
+                bay: "",
+            }),
+        };
     }
 
-    Ok(())
+    Ok(jobs)
 }
 
-fn date(date: DataType) -> String {
-    match date.as_date() {
+fn parse_job(job: &str) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(\w)-(\d+)\w?-(\d+)").unwrap();
+    }
+
+    match RE.captures(job) {
+        Some(caps) => format!("{}-{}-{}", &caps[1], &caps[2], &caps[3]),
+        None => String::from(""),
+    }
+}
+
+fn date(date: Option<NaiveDate>) -> String {
+    match date {
         Some(x) => format!("{:}", x),
         None => String::from(""),
     }
@@ -29,7 +65,16 @@ fn date(date: DataType) -> String {
 fn main() -> Result<(), Error> {
     println!("Rust excel!");
 
-    let _ = example();
+    let jobs = get_data().unwrap();
+    for elem in jobs {
+        // println!("{:?}", elem);
+        println!(
+            "{:<13} | {:<10} | {}",
+            elem.name,
+            date(elem.early_start),
+            date(elem.main_start)
+        );
+    }
 
     Ok(())
 }
